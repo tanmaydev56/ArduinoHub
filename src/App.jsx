19f288a92f07a37287569cbc84d365a9225610c1
Tanmay <tanmaysharma763@gmail.com>
2025-03-23 03:50:34 +0530

    firstProject3

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route,  useParams, useNavigate } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, useNavigate, useLocation } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Main from "./components/Main";
 import Application from "./components/Application";
@@ -13,21 +13,25 @@ import Articles from "./pages/Blogpage";
 import MainDashboard from "./pages/MainDashboard";
 
 function App() {
-  // const { slug } = useParams();
   const navigate = useNavigate();
+  const location = useLocation();
+
   return (
-    
-      <>
+    <>
       <Navbar />
-      <button onClick={() => navigate(-1)} className=" bg-blue-600 absolute left-10 top-20 font-bold mb-4 cursor-pointer text-white rounded-full px-3 py-2 flex items-center">
-        ← Back
-      </button>
+      {location.pathname !== "/" && (
+        <button
+          onClick={() => navigate(-1)}
+          className="bg-blue-600 absolute left-10 top-20 font-bold mb-4 cursor-pointer text-white rounded-full px-3 py-2 flex items-center"
+        >
+          ← Back
+        </button>
+      )}
       <Routes>
         <Route
           path="/articles/lpg-robot"
           element={
             <>
-             
               <Main />
               <Application />
               <SuppliesDesc />
@@ -38,11 +42,11 @@ function App() {
             </>
           }
         />
-      <Route path="/articles" element={<Articles />} />
+        <Route path="/articles" element={<Articles />} />
         <Route path="/articles/:id" element={<BlogDetail />} />
         <Route path="/" element={<MainDashboard />} />
       </Routes>
-      </>
+    </>
   );
 }
 
