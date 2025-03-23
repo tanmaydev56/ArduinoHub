c3e0652d5434ca9f7a693743ec2dbf0eac9f1106
Tanmay <tanmaysharma763@gmail.com>
2025-03-23 22:49:55 +0530

    daw

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,14 @@
 import React from "react";
-// import { Link } from "react-router-dom";
-import { Link } from "react-router-dom";
 
+import { NavLink } from "react-router-dom";
 const Navbar = () => {
   return (
     <div className="bg-blue-600 text-white w-full p-4 flex justify-between items-center shadow-md">
       <h1 className="text-2xl font-bold"> ArduinoHub</h1>
       <nav>
-      <a href="/articles"
+      <NavLink to="/articles"
       className="text-white hover:text-gray-300 text-xl mr-4">
-      Article</a>
+      Article</NavLink>
       </nav>
     </div>
   );
